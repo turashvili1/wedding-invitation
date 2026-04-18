0aa2235cda3579d1809ed22c6f03b9f8124b7c08
turashvili1 <135813954+turashvili1@users.noreply.github.com>
2026-04-18 17:27:23 +0400

    Update CongratulatoryMoney.tsx

--- src/components/CongratulatoryMoney.tsx
+++ src/components/CongratulatoryMoney.tsx
@@ -1,14 +1,14 @@
-import { CheckCircleTwoTone } from "@ant-design/icons";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import { styled } from "@stitches/react";
-import { Button, Divider, Modal, message } from "antd";
+import { Button, Divider, Message, Modal } from "antd";
 import { useState } from "react";
-import CopyToClipboard from "react-copy-to-clipboard";
+import data from "../data.json";
 
 const Wrapper = styled("div", {
   background: "#efebe9",
   backgroundImage: "url(./assets/GroovePaper.png)",
-  paddingBottom: 18,
   width: "100%",
+  paddingBottom: 42,
   textAlign: "center",
 });
 
@@ -20,190 +20,134 @@ const Title = styled("p", {
 });
 
 const Content = styled("p", {
-  fontSize: "1.5vh",
-  lineHeight: 1.75,
-  opacity: 0.75,
-  marginBottom: 24,
-});
-
-const SubContent = styled("p", {
-  fontSize: "1.3vh",
+  fontSize: "1.75vh",
   lineHeight: 1.75,
   opacity: 0.75,
   marginBottom: 24,
 });
 
 const Description = styled("p", {
-  fontSize: "1.3vh",
+  fontSize: "1.75vh",
   lineHeight: 1.75,
   opacity: 0.65,
   marginTop: 8,
 });
 
-const ContactButton = styled("div", {
-  display: "inline-block",
-  textAlign: "center",
-  marginLeft: 24,
-  marginRight: 24,
-  marginBottom: 24,
-});
-
-type CongratulatoryMoneyProps = {
-  data?: Data;
-};
-
-export default function CongratulatoryMoney({
-  data,
-}: CongratulatoryMoneyProps) {
-  const [groomVisible, setGroomVisible] = useState<boolean>(false);
-  const [brideVisible, setBrideVisible] = useState<boolean>(false);
+export default function CongratulatoryMoney() {
+  const [isGroomVisible, setIsGroomVisible] = useState<boolean>(false);
+  const [isBrideVisible, setIsBrideVisible] = useState<boolean>(false);
 
   return (
     <Wrapper>
       <Divider plain style={{ marginTop: 0, marginBottom: 32 }}>
-        <Title>축하의 마음을 전하세요</Title>
+        <Title>საჩუქრები</Title>
       </Divider>
-      <Content>축하의 마음을 담아 축의금을 전달해 보세요.</Content>
-      <ContactButton onClick={() => setGroomVisible(true)}>
-        <CheckCircleTwoTone
-          style={{ fontSize: 64, marginBottom: 16 }}
-          twoToneColor="#829fe0"
-        />
-        <br />
-        <SubContent>신랑측 계좌번호 확인</SubContent>
-      </ContactButton>
-      <ContactButton onClick={() => setBrideVisible(true)}>
-        <CheckCircleTwoTone
-          style={{ fontSize: 64, marginBottom: 16 }}
-          twoToneColor="#fe7daf"
-        />
-        <br />
-        <SubContent>신부측 계좌번호 확인</SubContent>
-      </ContactButton>
+      <Content>
+        თუ გსურთ სიხარული საჩუქრით გამოხატოთ, <br />
+        შეგიძლიათ ისარგებლოთ საბანკო რეკვიზიტებით.
+      </Content>
+      <Button
+        onClick={() => setIsGroomVisible(true)}
+        style={{ background: "#829fe0", border: "none", color: "white" }}
+      >
+        სიძის მხარე
+      </Button>
+      <Button
+        onClick={() => setIsBrideVisible(true)}
+        style={{ background: "#e082b1", border: "none", color: "white", marginLeft: 8 }}
+      >
+        პატარძლის მხარე
+      </Button>
       <Modal
-        title={<b>신랑측 계좌번호</b>}
-        open={groomVisible}
-        onOk={() => setGroomVisible(false)}
-        onCancel={() => setGroomVisible(false)}
+        title="სიძის მხარის საბანკო რეკვიზიტები"
+        open={isGroomVisible}
+        onOk={() => setIsGroomVisible(false)}
+        onCancel={() => setIsGroomVisible(false)}
         cancelButtonProps={{ style: { display: "none" } }}
         okButtonProps={{ style: { display: "none" } }}
         footer={null}
       >
-        {data?.groom?.parents?.father && (
-          <div>
-            <b>부) {data?.groom?.parents?.father?.name}</b>
-            <Divider type="vertical" />
-            <CopyToClipboard
-              text={data?.groom?.parents?.father?.account_number}
-            >
-              <Button
-                type="text"
-                style={{ padding: 0, margin: 0 }}
-                onClick={() => message.success("계좌번호가 복사되었습니다.")}
-              >
-                {data?.groom?.parents?.father?.account_number}
-              </Button>
-            </CopyToClipboard>
-          </div>
-        )}
-        {data?.groom?.parents?.mother && (
-          <div style={{ marginTop: 24, marginBottom: 24 }}>
-            <b>모) {data?.groom?.parents?.mother.name}</b>
-            <Divider type="vertical" />
-            <CopyToClipboard text={data?.groom?.parents?.mother.account_number}>
-              <Button
-                type="text"
-                style={{ padding: 0, margin: 0 }}
-                onClick={() => message.success("계좌번호가 복사되었습니다.")}
-              >
-                {data?.groom?.parents?.mother.account_number}
-              </Button>
-            </CopyToClipboard>
-          </div>
-        )}
-        {data?.groom && (
-          <div>
-            <b>신랑 {data?.groom?.name}</b>
-            <Divider type="vertical" />
-            <CopyToClipboard text={data?.groom?.account_number}>
-              <Button
-                type="text"
-                style={{ padding: 0, margin: 0 }}
-                onClick={() => message.success("계좌번호가 복사되었습니다.")}
-              >
-                {data?.groom?.account_number}
-              </Button>
-            </CopyToClipboard>
-          </div>
-        )}
         <div>
-          <Description>
-            계좌번호 클릭시, 붙여넣기 가능한 텍스트로 복사됩니다.
-          </Description>
+          <b>{data.groom.name}</b>
+          <Description>{data.groom.account_number}</Description>
+          <CopyToClipboard
+            text={data.groom.account_number}
+            onCopy={() => Message.success("ანგარიშის ნომერი დაკოპირებულია")}
+          >
+            <Button type="primary" size="small" style={{ marginBottom: 16 }}>
+              კოპირება
+            </Button>
+          </CopyToClipboard>
+        </div>
+        <div>
+          <b>დედა: {data.groom.parents.mother.name}</b>
+          <Description>{data.groom.parents.mother.account_number}</Description>
+          <CopyToClipboard
+            text={data.groom.parents.mother.account_number}
+            onCopy={() => Message.success("ანგარიშის ნომერი დაკოპირებულია")}
+          >
+            <Button type="primary" size="small" style={{ marginBottom: 16 }}>
+              კოპირება
+            </Button>
+          </CopyToClipboard>
+        </div>
+        <div>
+          <b>მამა: {data.groom.parents.father.name}</b>
+          <Description>{data.groom.parents.father.account_number}</Description>
+          <CopyToClipboard
+            text={data.groom.parents.father.account_number}
+            onCopy={() => Message.success("ანგარიშის ნომერი დაკოპირებულია")}
+          >
+            <Button type="primary" size="small">
+              კოპირება
+            </Button>
+          </CopyToClipboard>
         </div>
       </Modal>
       <Modal
-        title={<b>신부측 계좌번호</b>}
-        open={brideVisible}
-        onOk={() => setBrideVisible(false)}
-        onCancel={() => setBrideVisible(false)}
+        title="პატარძლის მხარის საბანკო რეკვიზიტები"
+        open={isBrideVisible}
+        onOk={() => setIsBrideVisible(false)}
+        onCancel={() => setIsBrideVisible(false)}
         cancelButtonProps={{ style: { display: "none" } }}
         okButtonProps={{ style: { display: "none" } }}
         footer={null}
       >
-        {data?.bride?.parents?.father && (
-          <div>
-            <b>부) {data?.bride?.parents?.father?.name}</b>
-            <Divider type="vertical" />
-            <CopyToClipboard
-              text={data?.bride?.parents?.father?.account_number}
-            >
-              <Button
-                type="text"
-                style={{ padding: 0, margin: 0 }}
-                onClick={() => message.success("계좌번호가 복사되었습니다.")}
-              >
-                {data?.bride?.parents?.father?.account_number}
-              </Button>
-            </CopyToClipboard>
-          </div>
-        )}
-        {data?.bride?.parents?.mother && (
-          <div style={{ marginTop: 24, marginBottom: 24 }}>
-            <b>모) {data?.bride?.parents?.mother?.name}</b>
-            <Divider type="vertical" />
-            <CopyToClipboard
-              text={data?.bride?.parents?.mother?.account_number}
-            >
-              <Button
-                type="text"
-                style={{ padding: 0, margin: 0 }}
-                onClick={() => message.success("계좌번호가 복사되었습니다.")}
-              >
-                {data?.bride?.parents?.mother?.account_number}
-              </Button>
-            </CopyToClipboard>
-          </div>
-        )}
-        {data?.bride && (
-          <div>
-            <b>신부 {data?.bride?.name}</b>
-            <Divider type="vertical" />
-            <CopyToClipboard text={data?.bride?.account_number}>
-              <Button
-                type="text"
-                style={{ padding: 0, margin: 0 }}
-                onClick={() => message.success("계좌번호가 복사되었습니다.")}
-              >
-                {data?.bride?.account_number}
-              </Button>
-            </CopyToClipboard>
-          </div>
-        )}
         <div>
-          <Description>
-            계좌번호 클릭시, 붙여넣기 가능한 텍스트로 복사됩니다.
-          </Description>
+          <b>{data.bride.name}</b>
+          <Description>{data.bride.account_number}</Description>
+          <CopyToClipboard
+            text={data.bride.account_number}
+            onCopy={() => Message.success("ანგარიშის ნომერი დაკოპირებულია")}
+          >
+            <Button type="primary" size="small" style={{ marginBottom: 16 }}>
+              კოპირება
+            </Button>
+          </CopyToClipboard>
+        </div>
+        <div>
+          <b>დედა: {data.bride.parents.mother.name}</b>
+          <Description>{data.bride.parents.mother.account_number}</Description>
+          <CopyToClipboard
+            text={data.bride.parents.mother.account_number}
+            onCopy={() => Message.success("ანგარიშის ნომერი დაკოპირებულია")}
+          >
+            <Button type="primary" size="small" style={{ marginBottom: 16 }}>
+              კოპირება
+            </Button>
+          </CopyToClipboard>
+        </div>
+        <div>
+          <b>მამა: {data.bride.parents.father.name}</b>
+          <Description>{data.bride.parents.father.account_number}</Description>
+          <CopyToClipboard
+            text={data.bride.parents.father.account_number}
+            onCopy={() => Message.success("ანგარიშის ნომერი დაკოპირებულია")}
+          >
+            <Button type="primary" size="small">
+              კოპირება
+            </Button>
+          </CopyToClipboard>
         </div>
       </Modal>
     </Wrapper>

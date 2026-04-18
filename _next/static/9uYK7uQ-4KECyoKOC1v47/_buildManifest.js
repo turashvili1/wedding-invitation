859447e58b60467cf958a285988e04bcaf41b4de
turashvili1 <135813954+turashvili1@users.noreply.github.com>
2026-04-18 22:56:51 +0900

    Deploying to gh-pages from @ turashvili1/wedding-invitation@f6e731caf1432a67aabd5187633dbebc5503dff0 🚀

--- _next/static/9uYK7uQ-4KECyoKOC1v47/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/7491423f9a145549.css","static/chunks/pages/index-951bb5aebc33a0ef.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
